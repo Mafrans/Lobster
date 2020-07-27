94c4f4612910fa82d56494106dec2fac6585fe50
Mafrans <malte.kluft@gmail.com>
2020-07-27 20:19:50 +0200

    Give the challenge command a name

--- src/commands/ChallengeCommand.ts
+++ src/commands/ChallengeCommand.ts
@@ -2,11 +2,10 @@ import { ICommand } from "./ICommand";
 import {Message, User} from "discord.js";
 
 export class ChallengeCommand implements ICommand {
-    aliases: string[];
-    name: string;
+    aliases: string[] = [];
+    name: string = 'challenge';
 
     run(cmd: string, author: User, args: string[], message: Message): Promise<any> {
-        console.log({cmd, author, args, message});
         return Promise.resolve(undefined);
     }
 
