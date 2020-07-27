9b6f4720f72ae6d39875d3be0e722d190e3322ee
Mafrans <malte.kluft@gmail.com>
2020-07-27 19:47:02 +0200

    Add command manager

---
+++ src/commands/CommandManager.ts
@@ -0,0 +1,20 @@
+import {ICommand} from "./ICommand";
+
+class CommandManager {
+    private commands: ICommand[];
+
+    register(command: ICommand): void {
+        if(this.commands.includes(command)) {
+            console.log(`⚠ Command ${command.name} registered twice.`);
+            return;
+        }
+
+        this.commands.push(command);
+    }
+
+    find(name: string): ICommand {
+        return this.commands.find(e => e.name.toLowerCase() === name.toLowerCase() || e.aliases.includes(name.toLowerCase()));
+    }
+}
+
+export default new CommandManager();
