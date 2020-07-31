cde6ea2b2829c4d2ddc6de6e80e2c25158c2ad16
Mafrans <malte.kluft@gmail.com>
2020-08-01 00:19:10 +0200

    Hosts work proper now!

--- src/ArtChallengeManager.ts
+++ src/ArtChallengeManager.ts
@@ -1,6 +1,9 @@
 import Nedb = require("nedb");
 import * as path from "path";
 import {ArtChallenge, ArtChallengeResult, ArtChallengeSubmission} from "./ArtChallenge";
+import {Lobster} from "./Lobster";
+import MessageListener from "./listeners/MessageListener";
+import {TextChannel} from "discord.js";
 
 class ArtChallengeManager {
     database: Nedb;
@@ -76,16 +79,59 @@ class ArtChallengeManager {
         })
     }
 
-    addSubmission(submission: ArtChallengeSubmission) {
-        this.database.insert(submission)
+    async addSubmission(submission: ArtChallengeSubmission) {
+        this.database.insert(submission);
+
+        const challenge = await this.getChallenge(submission.challenge);
+        const author = await Lobster.client.users.fetch(submission.author);
+
+        for(const hostId of challenge.hosts) {
+            const host = await Lobster.client.users.fetch(hostId);
+            const message = await host.dmChannel.send(`"${submission.title}" by ${author.username}`, {files: [submission.image]});
+
+            message.react('✅')
+
+            MessageListener.waitForReact({
+                message: message,
+                emoji: '✅',
+                users: [ host.id ],
+                type: 'add'
+            }).then(() => {
+                submission.accepted[host.id] = message.id;
+
+                if(Object.keys(submission.accepted).filter(k => submission.accepted[k] != null).length === Object.keys(submission.accepted).length) {
+                    Lobster.client.channels.fetch(challenge.submissionChannel)
+                        .then(channel => {
+                            (channel as TextChannel).send(`"${submission.title}" by ${author.username}`, {files: [submission.image]});
+                        });
+                }
+            });
+
+            MessageListener.waitForReact({
+                message: message,
+                emoji: '✅',
+                users: [ host.id ],
+                type: 'remove'
+            }).then(() => {
+                submission.accepted[host.id] = null;
+            });
+        }
     }
 
-    removeSubmission(submission: ArtChallengeSubmission) {
-        this.database.find({ author: submission.author, challenge: submission.challenge }, function (err, doc) {
-            console.log(doc);
+    async removeSubmission(submission: ArtChallengeSubmission) {
+        const doc: ArtChallengeSubmission[] = await new Promise(resolve => this.database.find({ author: submission.author, challenge: submission.challenge }, function (err, doc) { resolve(doc) }));
+        console.log(doc);
 
-            this.database.remove({ author: submission.author, challenge: submission.challenge });
-        }.bind(this));
+        for(const sub of doc) {
+            for(const hostId of Object.keys(sub.accepted)) {
+                const host = await Lobster.client.users.fetch(hostId);
+                const message = await host.dmChannel.messages.fetch(sub.accepted[host.id]);
+
+                message.delete();
+            }
+        }
+
+        this.database.remove({ author: submission.author, challenge: submission.challenge });
     }
 }
 
